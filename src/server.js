aed540131ffc48299dcb8fbd06c3e926a92035e1
danieljrenfro <djrenfro95@gmail.com>
2021-01-11 04:47:04 -0600

    add pg.defaults.ssl=true

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const knex = require('knex');
 const { PORT, DATABASE_URL } = require('./config');
 
-// pg.defaults.ssl=true;
+pg.defaults.ssl=true;
 
 let db = knex({
   client: 'pg',

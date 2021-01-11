682d6f5e14db42ff1f6eeb4672b0efd16c1d7b6a
danieljrenfro <djrenfro95@gmail.com>
2021-01-11 05:40:02 -0600

    update cors setting

--- src/app.js
+++ src/app.js
@@ -20,11 +20,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+app.use(cors());
 
 app.use('/api/lists', listsRouter);
 app.use('/api/words', wordsRouter);
